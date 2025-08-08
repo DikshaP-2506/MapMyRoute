d72cf40860b2d088ea94a1062df238e84837216d
Diksha Patkar <dikshapatkar625@gmail.com>
2025-08-08 12:04:57 +0530

    chatbot

--- frontend/src/LandbotWidgetLoader.jsx
+++ frontend/src/LandbotWidgetLoader.jsx
@@ -8,6 +8,8 @@ const LandbotWidgetLoader = () => {
     script.type = "module";
     script.async = true;
     script.innerHTML = `
+      window.addEventListener('mouseover', initLandbot, { once: true });
+      window.addEventListener('touchstart', initLandbot, { once: true });
       var myLandbot;
       function initLandbot() {
         if (!myLandbot) {
@@ -16,7 +18,7 @@ const LandbotWidgetLoader = () => {
           s.async = true;
           s.addEventListener('load', function() {
             var myLandbot = new Landbot.Livechat({
-              configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3022502-8EU14B8QYAUP44MI/index.json',
+              configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3085247-WZ1II3L6TB0QYI30/index.json',
             });
           });
           s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
@@ -24,7 +26,6 @@ const LandbotWidgetLoader = () => {
           x.parentNode.insertBefore(s, x);
         }
       }
-      initLandbot();
     `;
     document.body.appendChild(script);
   }, []);
